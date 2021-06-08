ec46457e9cc3283b654676fc9aa31d12ed6a9f19
gordan <gordan790530@gmail.com>
2021-06-08 15:33:13 +0800

    3rd version

--- Githubsample/src/testapp/mainapp.java
+++ Githubsample/src/testapp/mainapp.java
@@ -6,6 +6,6 @@ public class mainapp {
 		// TODO Auto-generated method stub
     System.out.println("2nd version");
   //Â¾­û­×§ï
-    System.out.println("3nd version");
+    System.out.println("3nd version1");
 	}
 }
